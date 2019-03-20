16e1fafe4c8fa262247a0e8c7b41dcb9f3ad1eec
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-20 14:07:37 -0500

    Project setup

---
+++ main.js
@@ -0,0 +1,8 @@
+// Your job is to produce the following reports for the dealership based on their total 2017 sales.
+
+// Total profit for 2017
+// In which month did they sell the most cars?
+// Which salesperson sold the most cars?
+// Which salesperson made the most profit?
+// Which model was the most popular?
+// Which bank provided the most loans to our customers?
